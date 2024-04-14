9e70a61f40eab2dc6bb071fb04a6202c5cbb965c
deepak-jha-2003 <deepakjha14052003@gmail.com>
2024-04-14 14:50:21 +0530

    util code practice

---
+++ src/utils/asyncHandelard.js
@@ -0,0 +1,28 @@
+const asyncHandle = (requestHandler) => {
+    (req , res , next) => {
+        Promise.resolve(requestHandler(req, res , next))
+        .catch((err) => next(err))
+    }
+}
+
+export {asyncHandle}
+
+
+
+
+
+// by the help of try catch method
+
+// const asyncHandle = (fn) => async (req , res , next) => {
+//     try {
+//         await fn(req , res , next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             Success : false,
+//             message : err.message
+//         })
+        
+//     }
+}
+
+}
